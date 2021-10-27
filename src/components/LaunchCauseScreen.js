aabfafc9c1a0178e4cb086b97a86c624bdba58c2
Yoann Danset <ydanset@c1r3p9.42nice.fr>
2021-10-27 18:42:05 +0200

    ;P

--- src/components/LaunchCauseScreen.js
+++ src/components/LaunchCauseScreen.js
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
-import { Container, Row, Col, Card, Form, InputGroup } from "react-bootstrap";
-import { faQuestionCircle } from "@fortawesome/free-regular-svg-icons";
+import { Container, Row, Col, Card, Form, InputGroup, OverlayTrigger } from "react-bootstrap";
+import { faInfoCircle, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 import "../styles/Petition.scss";
 import Navbar from "./Navbar";
@@ -9,6 +9,9 @@ import InYourArea from "./InYourArea";
 import SuccessStories from "./SuccessStories";
 import Features from "./Features";
 import KeyChangeMakers from "./KeyChangeMakers";
+import CluePopover from "./CluePopover";
+import PetitionInput from "./PetitionInput";
+import RichTextEditor from "./RichTextEditor";
 
 const issuesContent = [
 	{
@@ -45,6 +48,70 @@ const issuesContent = [
 	}
 ];
 
+const titleClues = [
+	{
+		clue: "Keep it short to the point",
+		details: 'Example “Buy organic, free-range eggs for your restaurants” Not: “Stop the inhumane treatment of chickens inbattery farms that are forced…”'
+	},
+	{
+		clue: "Focus on the solution",
+		details: "Example: “Raise theminimum wage in manchester to $15 an hour”. Not: “Stop rising inequality in Manchester”"
+	},
+	{
+		clue: "Communicate urgency",
+		details: "Example:“Approuve life-saving medication for my daughter’s before it’s too late”;"
+	},
+];
+
+const targetClues = [
+	{
+		clue: "Many petitions have companies or businesses as targets",
+		details: 'Examples: “Microsoft” or “Divya’s Beauty Salon”. Feel free to also list the CEO or another company official.'
+	},
+	{
+		clue: "Politicians, legislatures, or government agencies are also common",
+		details: "Examples: “Mumbai City Council”, your Sabha representative’s name, or the Ministry of Health and Family Welfare"
+	},
+	{
+		clue: "Don’t overthink it",
+		details: "You can add or change targets later. For now, just make your best guess and write the rest of your petition!"
+	},
+];
+
+const descriptionClues = [
+	{
+		clue: "Describe the people involved and the problem they are facing",
+		details: 'Readers are most likely to take action when they understand who is affected. '
+	},
+	{
+		clue: "Describe the solution",
+		details: "Explain what needs to happen and who can make the change. Make it clear what happens if you win or lose."
+	},
+	{
+		clue: "Make it personal",
+		details: "Readers are more likely to sign and support your petition if it’s clear why you care."
+	},
+	{
+		clue: "Respect others",
+		details: "Don't bully, use hate speech, threaten violence or make things up."
+	}
+];
+
+const mediaClues = [
+	{
+		clue: "Choose a photo that captures the emotion of your petition",
+		details: 'Photos of people or animals work well.'
+	},
+	{
+		clue: "Try to upload photos that are 1600 x 900 pixels or larger",
+		details: "Large photos look good on all screen sizes."
+	},
+	{
+		clue: "Keep it friendly for all audiences",
+		details: "Make sure your photo doesn't include graphic violence or sexual content."
+	},
+];
+
 const PetitionTitle = ({ title, subtitle }) => {
 	return (
 		<div className="petition-title-wrapper">
@@ -58,46 +125,50 @@ const PetitionTitle = ({ title, subtitle }) => {
 	);
 }
 
-const FilesInput = () => {
-	const [files, setFiles] = useState([]);
-	const [urls, setUrls] = useState([]);
+// RETAFF DESSUS
+const FilesInput = ({ state, setState }) => {
+	const [urls, setUrls] = useState("");
 	
 	const onChange = (e) => {
 		let filesArr = Array.prototype.slice.call(e.target.files);
-		setFiles([...filesArr]);
+		setState({ ...state, files: filesArr });
 	}
 
 	return (
 		<div className="files-input">
+			<OverlayTrigger trigger={["hover", "trigger"]} placement="bottom-end" overlay={CluePopover(mediaClues)} delay={0}>
+				<InputGroup.Text className="petition-media-clue">
+					<FontAwesomeIcon icon={faInfoCircle} style={{fontSize: "1.2rem"}} />
+				</InputGroup.Text>
+			</OverlayTrigger>
 			<label className="custom-file-upload">
 				<input type="file" multiple onChange={onChange} />
 				Choose a file
 			</label>
 			<div className="files-input-border"></div>
-			
-			<InputGroup className="files-input-group">
-				<Form.Control
-					className="files-input-url"
-					type="text"
-					value={urls}
-					onChange={e => setUrls([...urls, e.target.value])}
-					placeholder="http://"
-				/>
-				<InputGroup.Text className="files-input-btn">Submit</InputGroup.Text>
-			</InputGroup>
-			{files.map(file => (
+			<PetitionInput
+				placeholder="http://"
+				clues={mediaClues}
+				btnTxt="Add"
+				state={urls}
+				setState={e => setUrls(e.target.value)}
+			/>
+			{state.files.map(file => (
 				<div>{file.name}</div>
 			))}
 		</div>
 	);
 }
 
-const LaunchCauseScreen = () => {
-	const [data, setData] = useState({
+const SignalRiskScreen = () => {
+	const [state, setState] = useState({
 		issueSelected: null,
 		keywords: "",
-		goal: "",
+		title: "",
 		target: "",
+		description: "",
+		files: [],
+		urls: [],
 		keywordsMedia: ""
 	});
 
@@ -114,9 +185,9 @@ const LaunchCauseScreen = () => {
 						{issuesContent.map((item, index) => (
 							<Col 
 								md={3}
-								className={`petition-issue-container ${index ===data.issueSelected ? "petition-issue-container-active" : null}`} 
+								className={`petition-issue-container ${index ===state.issueSelected ? "petition-issue-container-active" : null}`} 
 								key={`petition-issue-${index}`}
-								onClick={e => setData({ ...data, issueSelected: index })}
+								onClick={e => setState({ ...state, issueSelected: index })}
 							>
 								<Card className="petition-issue">
 									<Card.Img variant="top" src={item.img} style={{borderRadius: "50%"}} />
@@ -127,15 +198,20 @@ const LaunchCauseScreen = () => {
 							</Col>
 						))}
 					</Row>
-					<Row className="justify-content-center mt-5 mb-5">
+					<Row className="justify-content-center mt-4 mb-5">
 						<Col md={8}>
-							<Form.Control
-								className="petition-keywords"
-								type="text"
-								value={data.keywords}
-								onChange={e => setData({ ...data, keywords: e.target.value })}
-								placeholder="Search by keywords: #Free Speech, democracy"
-							/>
+							<InputGroup>
+								<Form.Control
+									className="petition-keywords"
+									type="text"
+									value={state.keywords}
+									onChange={e => setState({ ...state, keywords: e.target.value })}
+									placeholder="Search by keywords: #Free Speech, democracy"
+								/>
+								<InputGroup.Text className="petition-input-clue">
+									<FontAwesomeIcon icon={faSearch} style={{fontSize: "1.2rem"}} />
+								</InputGroup.Text>
+							</InputGroup>
 						</Col>
 					</Row>
 					<Row className="justify-content-center petition-form-block">
@@ -144,18 +220,27 @@ const LaunchCauseScreen = () => {
 								title="Write Your Cause Title"
 								subtitle="this is the first thing people will see about your risk. Get their attention with short title that focus on the change you'd like them to support"
 							/>
-							<InputGroup className="mt-4">
-								<Form.Control
-									className="petition-goal"
-									type="text"
-									value={data.goal}
-									onChange={e => setData({ ...data, goal: e.target.value })}
-									placeholder="What do you whant to achieve?"
-								/>
-								<InputGroup.Text style={{background: "transparent"}}>
-									<FontAwesomeIcon icon={faQuestionCircle} style={{fontSize: "1.2rem"}} />
-								</InputGroup.Text>
-							</InputGroup>
+							<PetitionInput
+								placeholder="What do you whant to achieve?"
+								clues={titleClues}
+								state={state.title}
+								setState={e => setState({ ...state, title: e.target.value})}
+								btnTxt="Continue"
+								showClues={true}
+							/>
+						</Col>
+					</Row>
+					<Row className="justify-content-center petition-form-block">
+						<Col>
+							<PetitionTitle
+								title="Describe the problem you want to solve"
+								subtitle="People are more likely to support your cause if it’s clear why you care. explain how this change will impact you, your family or you community."
+							/>
+							<RichTextEditor
+								state={state}
+								setState={setState}
+								clues={descriptionClues}
+							/>
 						</Col>
 					</Row>
 					<Row className="justify-content-center petition-form-block">
@@ -164,18 +249,14 @@ const LaunchCauseScreen = () => {
 								title="Great! Who Has The Power To Make This Change?"
 								subtitle="Choose the recipient(s) of your petition. These are people or organisations with the power to solve your problem or take the action you’re demanding."
 							/>
-							<InputGroup className="mt-4">
-								<Form.Control
-									className="petition-target"
-									type="text"
-									value={data.target}
-									onChange={e => setData({ ...data, target: e.target.value })}
-									placeholder="Petition target (e.g mayor)"
-								/>
-								<InputGroup.Text style={{background: "transparent"}}>
-									<FontAwesomeIcon icon={faQuestionCircle} style={{fontSize: "1.2rem"}} />
-								</InputGroup.Text>
-							</InputGroup>
+							<PetitionInput
+								placeholder="Petition target (e.g mayor)"
+								clues={targetClues}
+								state={state.target}
+								setState={e => setState({ ...state, target: e.target.value})}
+								btnTxt="Continue"
+								showClues={true}
+							/>
 						</Col>
 					</Row>
 					<Row className="justify-content-center petition-form-block">
@@ -184,17 +265,18 @@ const LaunchCauseScreen = () => {
 								title="Add A Photo Or Video"
 								subtitle="Petitions with a photo or video receive six times more signatures than those without. Include one that captures the emotion of your story."
 							/>
-							<FilesInput />
+							<FilesInput state={state} setState={setState}/>
 						</Col>
 					</Row>
 					<Row className="justify-content-center mt-4">
 						<Col>
-							<Form.Control
-								className="petition-keywords-media"
-								type="text"
-								value={data.keywordsMedia}
-								onChange={e => setData({ ...data, keywordsMedia: e.target.value })}
-								placeholder="Add Keywords: #Free Speech, democracy"
+							<PetitionInput
+								placeholder="Add keywords: #Free speech, democracy, #sovreignty"
+								clues={targetClues}
+								state={state.keywordsMedia}
+								setState={e => setState({ ...state, keywordsMedia: e.target.value})}
+								btnTxt="Add"
+								btnClass="petition-keywords-media-btn"
 							/>
 						</Col>
 					</Row>
@@ -213,4 +295,4 @@ const LaunchCauseScreen = () => {
 	);
 }
 
-export default LaunchCauseScreen;
+export default SignalRiskScreen;
--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import { Container, Row, Col, Form } from "react-bootstrap";
+
+import "../styles/LoginScreen.scss";
+//import Navbar from "./Navbar";
+
 const LoginScreen = () => {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
 	return (
-		<div>
-			Login
-		</div>
+		<Container fluid style={{marginTop: "120px"}}>
+			<Row className="justify-content-center">
+				<Col xs={10} md={6}>
+					<div className="login-screen-title"></div>
+					<Form.Control
+						size="lg"
+						type="text"
+						value={username}
+						onChange={e => setUsername(e.target.value)}
+						placeholder="Username"
+					/>
+					<Form.Control
+						size="lg"
+						type="text"
+						value={password}
+						onChange={e => setPassword(e.target.value)}
+						placeholder="Password"
+					/>
+				</Col>
+			</Row>
+		</Container>
 	);
 }
 
