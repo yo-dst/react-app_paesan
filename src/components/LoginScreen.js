97684d0c4885e072351e7f930c262620ae4f3ef9
Yoann Danset <ydanset@c1r3p9.42nice.fr>
2021-10-27 21:33:35 +0200

    :p

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Container, Row, Col, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import "../styles/LoginScreen.scss";
 //import Navbar from "./Navbar";
@@ -10,18 +11,21 @@ const LoginScreen = () => {
 
 	return (
 		<Container fluid style={{marginTop: "120px"}}>
-			<Row className="justify-content-center">
+			<Row className="justify-content-center login-screen">
 				<Col xs={10} md={6}>
-					<div className="login-screen-title"></div>
+					<div className="login-screen-title">Login</div>
+					<div className="login-screen-title">
+						Not Registered Yet? <Link to="/register">Signup</Link>
+					</div>
 					<Form.Control
-						size="lg"
+						size="md"
 						type="text"
 						value={username}
 						onChange={e => setUsername(e.target.value)}
 						placeholder="Username"
 					/>
 					<Form.Control
-						size="lg"
+						size="md"
 						type="text"
 						value={password}
 						onChange={e => setPassword(e.target.value)}
